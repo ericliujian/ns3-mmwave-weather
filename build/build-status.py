ecb14525c03d5d6e4090032571597eb391b94a4b
ericliujian <juliu@mix.wvu.edu>
2022-04-19 09:25:03 -0400

    small change

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.33-5g_v2-debug', 'build/scratch/ns3.33-intersection_v1_1-debug', 'build/scratch/ns3.33-5g_v4-debug', 'build/scratch/ns3.33-4g_v1-debug', 'build/scratch/ns3.33-5g_v5-debug', 'build/scratch/ns3.33-5g_v3-debug', 'build/scratch/ns3.33-5g_v1-debug', 'build/scratch/ns3.33-5g-debug', 'build/scratch/subdir/ns3.33-subdir-debug']
+ns3_runnable_programs = ['build/scratch/ns3.33-5g_v6-debug', 'build/scratch/ns3.33-intersection_v1_1-debug', 'build/scratch/ns3.33-5g_v3-debug', 'build/scratch/ns3.33-5g-debug', 'build/scratch/ns3.33-4g_v1-debug', 'build/scratch/ns3.33-5g_v4-debug', 'build/scratch/ns3.33-5g_v5-debug', 'build/scratch/ns3.33-5g_v1-debug', 'build/scratch/ns3.33-5g_v2-debug', 'build/scratch/subdir/ns3.33-subdir-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
